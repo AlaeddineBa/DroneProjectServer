0e2a0d509badc879b7042811e6b14e25c9c17503
SalmaBouri <bouri.salma@gmail.com>
2017-02-14 15:13:23 +0100

    test add x position drone via API

--- app.js
+++ app.js
@@ -1,47 +1,3 @@
-/*
-var express = require('express');
-var app = express();
-//var mongojs = require('mongojs');
-
-var bodyParser = require('body-parser');
-
-
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.json());
-
-app.get('/test', function (req, res){
-  var test = {
-    test:"TEST"
-  };
-  res.json(test);
-});
-
-
-// Connect to the db
-// Retrieve
-var MongoClient = require('mongodb').MongoClient;
-
-// Connect to the db
-MongoClient.connect("mongodb://148.60.11.238:27018/admin", function(err, db) {
-  if(!err) {
-    var collection = db.collection('test');
-    var doc1 = {'hello':'doc1'};
-    var doc2 = {'hello':'doc2'};
-    var lotsOfDocs = [{'hello':'doc3'}, {'hello':'doc4'}];
-
-    collection.insert(doc1);
-    console.log(collection.find({dated:'2017-11-15 15:25:31.000Z'}).stream());
-
-    console.log("We are connected");
-  }
-  else
-    console.log(err);
-});
-
-app.listen(3000);
-console.log("Server running on Port 3000");
-*/
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -49,11 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var connect = require('./mongo/connectMongo');
+require('./mongo/connectMongo');
 var positionDrone = require('./model/positiondrone');
 var positionObjectif = require('./model/positionobjectif');
-var users = require('./routes/users');
 var positionTest = require('./routes/positionDrone');
 
 
@@ -71,8 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+
 app.use('/positiontest', positionTest);
 
 // catch 404 and forward to error handler
